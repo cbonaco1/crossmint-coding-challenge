3d1a0b55edd30b37fb989ba5e981bdfa37edc416
cbonaco1 <christian.bonacore@gmail.com>
2025-09-06 10:53:41 -0400

    feat: use async await when fetching goals

--- src/partTwo.ts
+++ src/partTwo.ts
@@ -4,31 +4,33 @@ const CANDIDATE_ID= "74b00bbc-16c7-4fa5-83ba-b4e2c4d0e9a3";
 import PlanetFactory from "./planets/PlanetFactory.js"; // NICE_TO_HAVE - remove extensions
 import Planet from "./planets/Planet.js";
 
-function main() {
+async function main() {
   const planets:Array<Planet> = [];
-  getGoal()
-    .then(({ data }) => {
-      if (data.goal) {
-        data.goal.forEach((rows:Array<string>, rowIndex: number) => {
-          // check if row is an array
-          rows.forEach((column:string, colIndex: number) => {
-            console.log("----")
-            console.log(`Row ${rowIndex} - Col ${colIndex} ---> ${column}`);
-            const planet = PlanetFactory.createPlanet(column);
-            if (planet) {
-              planets.push(planet);
-            }
-          })
-        });
-      }
-      console.log(`Creating ${planets.length} planets`)
-    })
-    .catch((err) => {
-      console.log("Some error: ", err.response.data)
-      console.log("Status: ", err.response.status)
-    })
-
-    
+  try {
+    const { data } = await getGoal();
+    if (data.goal) {
+      data.goal.forEach((rows:Array<string>, rowIndex: number) => {
+        // check if row is an array
+        rows.forEach((column:string, colIndex: number) => {
+          console.log("----")
+          console.log(`Row ${rowIndex} - Col ${colIndex} ---> ${column}`);
+          const planet = PlanetFactory.createPlanet(column);
+          if (planet) {
+            planets.push(planet);
+          }
+        })
+      });
+    }
+    console.log(`Creating ${planets.length} planets`)
+  } catch (error) {
+    console.log("Error fetching goal:")
+    if (axios.isAxiosError(error)) {
+       console.log("Status:", error.response?.status);
+       console.log("Data: ", error.response?.data);
+    } else {
+      console.log(error);
+    }
+  }
 }
 
 const getGoal = async () => {
